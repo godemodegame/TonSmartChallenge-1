b9bbbeea100673c3f4778f5766e93dce411f8091
Kirill Kirilenko <kvkirilenko@alfabank.ru>
2023-02-20 12:45:53 +0400

    beautify contracts

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -1,12 +1,11 @@
-import { Address, beginCell, toNano } from 'ton-core';
+import { Address, toNano } from 'ton-core';
 import { Proxy } from '../wrappers/Proxy';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
     const proxy = Proxy.createFromConfig(
         {
-            address: Address.parse("kQC_UQzCLJniVUDD4AD_uM8_cf22T7-QIHTy3zzfogCtO_XK"),
-            counter: Math.floor(Math.random() * 10000)
+            address: Address.parse("kQC_UQzCLJniVUDD4AD_uM8_cf22T7-QIHTy3zzfogCtO_XK")
         }, 
         await compile('Proxy')
     );
